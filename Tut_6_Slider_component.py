88d2730046a83d98684575b2843f7870b193ad2f
Enjoy-Maity <64701616+Enjoy-Maity@users.noreply.github.com>
2023-04-17 16:32:06 +0530

    added comments

--- Tut_6_Slider_component.py
+++ Tut_6_Slider_component.py
@@ -3,11 +3,12 @@ from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QApplication, QSlider
 
 def value_viewer(data):
+    # Here as we are taking the data and representing it, this variable (data) takes the return value of the Qslider.
     print(f"You selected the value of :{data}")
 
 app = QApplication()
-slider = QSlider(Qt.Horizontal)
-vslider = QSlider(Qt.Vertical)
+slider = QSlider(Qt.Horizontal) # This method returns an integer value, the amount upto which the slider is slided
+vslider = QSlider(Qt.Vertical)  # This method returns an integer value, the amount upto which the slider is slided
 slider.setMinimum(1)
 slider.setMaximum(100)
 slider.setValue(25)
